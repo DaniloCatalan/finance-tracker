fd57faa83f0db5fde3d6cd0d376ac7b2bcc2a5f9
Danilo P Catalan <danilo.e.catalan@gmail.com>
2021-08-23 22:47:56 -0400

    Add my friends view with listing of followed friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   def my_portfolio
-    @user = current_user
+    #@user = current_user
     @tracked_stocks = current_user.stocks
+  end
+  def my_friends
+    @friends = current_user.friends
   end
 end
